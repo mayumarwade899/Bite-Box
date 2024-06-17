109aea1070799060282cbe6f897f79d63662c66b
Mayur Marwade <mayumarwade899@gmail.com>
2024-06-17 13:28:37 +0530

    category slice added

--- src/components/CategoryMenu.js
+++ src/components/CategoryMenu.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FoodData from "../data/FoodData";
 
 const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  }, []);
+
   return (
     <div className="m-6">
       <h3 className="text-xl font-semibold">Find the best Food!</h3>
@@ -9,21 +23,16 @@ const CategoryMenu = () => {
           All
         </button>
 
-        <button className="px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white">
-          Lunch
-        </button>
-
-        <button className="px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white">
-          Breakfast
-        </button>
-
-        <button className="px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white">
-          Dinner
-        </button>
-
-        <button className="px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white">
-          Snacks
-        </button>
+        {categories.map((category, index) => {
+          return (
+            <button
+              key={index}
+              className="px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white"
+            >
+              {category}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
